a8d979e2e3e83be07f5004014a4ce9a472332d53
dougzilla32 <dougstein@gmail.com>
2024-08-23 14:37:09 -0700

    Initial Commit

---
+++ AlienInvaders macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  AlienInvaders macOS
+//
+//  Created by Doug on 8/23/24.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
